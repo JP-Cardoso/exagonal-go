e6984ca3d1fddd99bfc1de0851dd766b62f3a2e4
JP-Cardoso <jhonpoal@hotmail.com>
2023-09-11 11:02:42 -0300

    feat: using and testing cobra-cli to create a customizable cli

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
-	db2 "go-hexagonal/adapters/db"
-	"go-hexagonal/application"
-)
+import "go-hexagonal/cmd"
 
 func main() {
-	//conexão db
-	db, _ := sql.Open("sqlite3", "db.sqlite")
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product Exemplo", 30)
-	
-	productService.Enable(product)
+	cmd.Execute()
 }
